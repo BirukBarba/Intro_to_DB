aa9f122d2773d425b1d84ac1a19955f5c2448e82
birukbarba <birukbarba@gmail.com>
2025-10-19 10:37:00 +0300

    edited

--- task_2.sql
+++ task_2.sql
@@ -4,9 +4,13 @@ CREATE TABLE IF NOT EXISTS Authors (
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
